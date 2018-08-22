902b2146697f96c66743a5babe379adb5dd26d03
ZarkoZivojnovic <zivojnovic@gmail.com>
2018-08-22 23:37:14 +0200

    first commit

---
+++ js/brodici.js
@@ -0,0 +1,132 @@
+const labels = {emptySpace: 0, ship: 1, reservedSpace: 2, selectedShip: 3};
+
+let player1 = {
+        name: "",
+        table: createTable(),
+        shipsPosition: {}
+    },
+    player2 = {
+        name: "enemy",
+        table: createTable(),
+        hits: {}
+    },
+    ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1],
+    input = document.getElementById("start");
+
+input.addEventListener("submit", (event) => {
+    player1.name = document.getElementById("username").value;
+    input.style.display = "none";
+    document.getElementById("table").style.display = "flex";
+    document.getElementById("playerOne").textContent = player1.name;
+    positionSetting();
+    event.preventDefault();
+});
+
+let allListeners = {
+    settingPosition: {
+        mouseDown: function (event) {
+            let targetProperties = tableSetup.createTargetProperties(event);
+            localStorage.setItem("property", JSON.stringify(targetProperties));
+            player1.table = tableSetup.colorSelectedShip(player1.table, player1.shipsPosition, targetProperties, "on");
+        },
+        mouseUp: function (event) {
+            let rotateOrMove = "rotate",
+                newPlace;
+            if (event.button === 0) {
+                rotateOrMove = "move";
+                newPlace = event.target.id.split("_");
+            }
+            let properties = {
+                rotateOrMove: rotateOrMove,
+                newPlace: newPlace,
+                table: player1.table,
+                shipPosition: player1.shipsPosition
+            };
+            let targetProperties = JSON.parse(localStorage.getItem("property")),
+                tableAndShip = tableSetup.rotateOrMoveShip(properties, targetProperties);
+            console.log(player1.table);
+            player1.table = tableAndShip.table;
+            player1.shipsPosition = tableAndShip.shipPosition;
+            console.log(player1.table);
+            player1.table = tableSetup.colorSelectedShip(properties.table, properties.shipPosition, targetProperties, "off");
+            localStorage.removeItem("property");
+        },
+        newRandomPosition: function () {
+            let random = tableSetup.randomShipsPosition(player1.table);
+            player1.table = random.myTable;
+            player1.shipsPosition = random.shipsOnTable;
+        },
+        confirmPosition: function () {
+            /*ajaxStorage.set(player1.name, player1.table, function () {
+            console.log("set table", player1.table);
+        });*/
+            startTheBattle();
+        }
+    }
+};
+
+function startTheBattle() {
+
+}
+
+function positionSetting() {
+    let random = tableSetup.randomShipsPosition(player1.table);
+    player1.table = random.myTable;
+    player1.shipsPosition = random.shipsOnTable;
+    setInterval(() => {
+        document.getElementById("player1").innerHTML = drawOnPage(player1.table, "Player1").outerHTML;
+        document.getElementById("player2").innerHTML = drawOnPage(player2.table, "Player2").outerHTML;
+    }, 100);
+    listenersForPositionSetting("player1");
+}
+
+function listenersForPositionSetting(id) {
+    let player = document.getElementById(id);
+    player.addEventListener('contextmenu', (event) => {
+        event.preventDefault();
+    }, false);
+    player.addEventListener("mousedown", allListeners.settingPosition.mouseDown);
+    player.addEventListener("mouseup", allListeners.settingPosition.mouseUp);
+    document.getElementById("newRandom").addEventListener("click", allListeners.settingPosition.newRandomPosition);
+    document.getElementById("confirmPosition").addEventListener("click", allListeners.settingPosition.confirmPosition);
+}
+
+function createTable() {
+    let table = [];
+    for (let hight = 0; hight < 10; hight++) {
+        table[hight] = [];
+        for (let width = 0; width < 10; width++) {
+            table[hight][width] = labels.emptySpace;
+        }
+    }
+    return table;
+}
+
+function drawOnPage(table, player) {
+    let tabla = document.createElement("table");
+    tabla.setAttribute("id", player);
+    for (let row = 0; row < table.length; row++) {
+        let rowInTable = document.createElement("tr");
+        for (let cell = 0; cell < table[row].length; cell++) {
+            let cellInRow = document.createElement("td");
+            if (table[row][cell] === labels.emptySpace || table[row][cell] === labels.reservedSpace) cellInRow.style.backgroundColor = "white";
+            if (table[row][cell] === labels.ship) cellInRow.style.backgroundColor = "black";
+            if (table[row][cell] === labels.selectedShip) cellInRow.style.border = "solid blue 1px";
+            if (table[row][cell] === labels.selectedShip) cellInRow.style.backgroundColor = "yellow";
+            cellInRow.setAttribute("id", row + "_" + cell);
+            rowInTable.appendChild(cellInRow);
+        }
+        tabla.appendChild(rowInTable);
+    }
+    return tabla;
+}
+
+function koIgraPrvi() {
+    let prviIgra = Math.ceil(Math.random() * 2);
+    if (prviIgra === 1) {
+        return player1;
+    } else {
+        return player2;
+    }
+}
+
---
+++ js/tableSetup.js
@@ -0,0 +1,230 @@
+let tableSetup = {
+    createTargetProperties: function (event) {
+        let targetProperties;
+        if (event.target !== event.currentTarget) {
+            for (let shipNumber in player1.shipsPosition) {
+                if (player1.shipsPosition.hasOwnProperty(shipNumber)){
+                    let ship = player1.shipsPosition[shipNumber];
+                    for (let shipElement of ship.position) {
+                        if (shipElement.toString() === event.target.id.split("_").toString()) {
+                            targetProperties = {
+                                oldPosition: ship.position,
+                                oldDirection: ship.direction,
+                                newPosition: 0,
+                                shipNumber: shipNumber,
+                                lengthOfShip: ship.position.length,
+                                indexOfElement: ship.position.indexOf(shipElement)
+                            };
+                        }
+                    }
+                }
+            }
+        }
+        return targetProperties;
+    },
+
+    colorSelectedShip: function (table, shipPosition, targetProperties, turnOnOrOff) {
+        let type;
+            turnOnOrOff === "on" ? type = labels.selectedShip : type = labels.ship;
+        let selectedShip = shipPosition[targetProperties.shipNumber].position;
+        for (let field = 0; field < selectedShip.length; field++) {
+            for (let index = 0; index < selectedShip[field].length; index++) {
+                table[selectedShip[field][0]][selectedShip[field][1]] = type;
+            }
+        }
+        return table;
+    },
+
+    rotateOrMoveShip: function (properties, targetProperties) {
+        if (properties.rotateOrMove === "rotate" ){
+            targetProperties.newPosition = this.createNewRotatedPosition(targetProperties)
+        } else if(properties.rotateOrMove === "move") {
+            targetProperties.newPosition = this.createNewMovedPosition(targetProperties, properties.newPlace);
+        }
+        let newTableAndShip = this.removeOldPosition(properties, targetProperties);
+        properties.table = newTableAndShip.table;
+        properties.shipPosition = newTableAndShip.shipPosition;
+        if (this.isPositionEmpty(properties.table, targetProperties) === true) {
+            let newTableAndShip = this.addTranslatedShip(properties, targetProperties);
+            properties.table = newTableAndShip.table;
+            properties.shipPosition = newTableAndShip.shipPosition;
+        } else {
+            let oldTableAndShip = this.returnOldPosition(properties, targetProperties);
+            properties.table= oldTableAndShip.table;
+            properties.shipPosition = oldTableAndShip.shipPosition;
+        }
+        return {table: properties.table, shipPosition: properties.shipPosition};
+    },
+
+    createNewMovedPosition: function (targetProperties, newPlace) {
+        let newPosition = [],
+            moveVertical = newPlace[0] - targetProperties.oldPosition[targetProperties.indexOfElement][0],
+            moveHorizontal = newPlace[1] - targetProperties.oldPosition[targetProperties.indexOfElement][1];
+
+        for (let index = 0; index < targetProperties.oldPosition.length; index++) {
+            newPosition[index] = [targetProperties.oldPosition[index][0] + moveVertical, targetProperties.oldPosition[index][1] + moveHorizontal];
+        }
+        return newPosition;
+    },
+
+    createNewRotatedPosition: function (obj) {
+        let newPosition = [],
+            increment;
+        if (obj.indexOfElement === 0) {
+            increment = 0
+        }
+        else if (obj.indexOfElement === 1) {
+            increment = -1
+        }
+        else if (obj.indexOfElement === 2) {
+            increment = -2
+        }
+        else if (obj.indexOfElement === 3) {
+            increment = -3
+        }
+        for (let index = 0; index < obj.oldPosition.length; index++) {
+            let cell = obj.oldPosition[index];
+            if (obj.oldDirection === "horizontal") {
+                newPosition[index] = [cell[0] + increment, cell[1] - increment];
+            } else if (obj.oldDirection === "vertical") {
+                newPosition[index] = [cell[0] - increment, cell[1] + increment];
+            }
+            if (newPosition[index][0] < 0 || newPosition[index][0] > 9 || newPosition[index][1] < 0 || newPosition[index][1] > 9) return;
+            increment++;
+        }
+        return newPosition;
+    },
+
+    addTranslatedShip: function (properties, obj) {
+        let newDirection;
+        if (properties.rotateOrMove === "rotate") {
+            obj.oldDirection === "vertical" ? newDirection = "horizontal" : newDirection = "vertical";
+        } else {
+            newDirection = obj.oldDirection;
+        }
+        properties.shipPosition[obj.shipNumber] = {
+            position: obj.newPosition,
+            direction: newDirection
+        };
+        for (let cell = 0; cell < obj.newPosition.length; cell++) {
+            properties.table[obj.newPosition[cell][0]][obj.newPosition[cell][1]] = labels.ship;
+        }
+        properties.table = this.updateReservedSpace(properties.table);
+        return {table: properties.table, shipPosition: properties.shipPosition};
+    },
+
+    removeOldPosition: function (properties, obj) {
+        delete properties.shipPosition[obj.shipNumber];
+        for (let cell = 0; cell < obj.oldPosition.length; cell++) {
+            properties.table[obj.oldPosition[cell][0]][obj.oldPosition[cell][1]] = labels.emptySpace;
+        }
+        properties.table = this.updateReservedSpace(properties.table);
+        return {table: properties.table, shipPosition:properties.shipPosition}
+    },
+
+    updateReservedSpace: function (table) {
+        for (let row = 0; row < table.length; row++) { //remove old reserved space
+            for (let column = 0; column < table[row].length; column++) {
+                if (table[row][column] === labels.selectedShip) table[row][column] = labels.ship;
+                if (table[row][column] !== labels.ship) table[row][column] = labels.emptySpace;
+            }
+        }
+        for (let ship in player1.shipsPosition) { //add new reserved space
+            let shipPosition = player1.shipsPosition[ship]["position"];
+            for (let position = 0; position < shipPosition.length; position++) {
+                table = this.addReservedSpace(table, shipPosition[position]);
+            }
+        }
+        return table;
+    },
+
+    returnOldPosition: function (properties, obj) {
+        properties.shipPosition[obj.shipNumber] = {
+            position: obj.oldPosition,
+            direction: obj.oldDirection
+        };
+        for (let cell = 0; cell < obj.oldPosition.length; cell++) {
+            properties.table[obj.oldPosition[cell][0]][obj.oldPosition[cell][1]] = labels.ship;
+        }
+        properties.table = this.updateReservedSpace(properties.table);
+        return {table:properties.table, shipPosition:properties.shipPosition};
+    },
+
+    isPositionEmpty: function (table, obj) {
+        let counter = 0;
+        if (typeof obj.newPosition === "undefined") return false;
+        for (let cell = 0; cell < obj.newPosition.length; cell++) {
+            let index = obj.newPosition[cell];
+            if (typeof table[index[0]][index[1]] === "undefined") {
+                return false;
+            }
+            if (table[index[0]][index[1]] === labels.emptySpace)
+                counter++;
+        }
+        return obj.newPosition.length === counter;
+    },
+
+    addReservedSpace: function (table, cell) {
+        for (let verticalPosition = -1; verticalPosition < 2; verticalPosition++) {
+            for (let horizontalPosition = -1; horizontalPosition < 2; horizontalPosition++) {
+                if (table[cell[0] + verticalPosition] === undefined || table[cell[0] + verticalPosition][cell[1] + horizontalPosition] === undefined) continue;
+                if (table[cell[0] + verticalPosition][cell[1] + horizontalPosition] !== labels.ship) {
+                    table[cell[0] + verticalPosition][cell[1] + horizontalPosition] = labels.reservedSpace;
+                }
+            }
+        }
+        return table;
+    },
+
+    randomShipsPosition: function (table) {
+        let myTable = createTable(),
+            shipsOnTable = [];
+
+        for (let ship = 0; ship < ships.length; ship++) {
+            let direction = Math.floor(Math.random() * 2) === 0 ? "horizontal" : "vertical",
+                row = Math.floor(Math.random() * 10),
+                cell = Math.floor(Math.random() * 10),
+                shipLength,
+                shipPosition = [],
+                counter = 0,
+                index;
+
+            if (direction === "horizontal") {
+                for (shipLength = 0; shipLength < ships[ship]; shipLength++) {
+                    while (cell + ships[ship] >= table.length) {
+                        row = Math.floor(Math.random() * 10);
+                        cell = Math.floor(Math.random() * 10);
+                    }
+                    shipPosition[shipLength] = [row, cell + shipLength];
+                }
+            } else if (direction === "vertical") {
+                for (shipLength = 0; shipLength < ships[ship]; shipLength++) {
+                    while (row + ships[ship] >= table[0].length) {
+                        row = Math.floor(Math.random() * 10);
+                        cell = Math.floor(Math.random() * 10);
+                    }
+                    shipPosition[shipLength] = [row + shipLength, cell];
+                }
+            }
+            for (index = 0; index < shipPosition.length; index++) {
+                if (myTable[shipPosition[index][0]][shipPosition[index][1]] === labels.emptySpace) {
+                    counter++;
+                }
+            }
+            if (counter === shipPosition.length) {
+                for (index = 0; index < shipPosition.length; index++) {
+                    let position = shipPosition[index];
+                    shipsOnTable["ship" + ship] = {
+                        position: shipPosition,
+                        direction: direction
+                    };
+                    myTable[position[0]][position[1]] = labels.ship;
+                    this.addReservedSpace(myTable, [position[0], position[1]]);
+                }
+            } else {
+                ship--;
+            }
+        }
+        return {myTable: myTable, shipsOnTable: shipsOnTable}
+    }
+};
